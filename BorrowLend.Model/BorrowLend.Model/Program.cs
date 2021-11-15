3b522b0fe3d961ae5e74bb01697b21b2ce47fd99
mimozova <94359362+mimozova@users.noreply.github.com>
2021-11-15 18:29:38 +0200

    Add files via upload

---
+++ BorrowLend.Model/BorrowLend.Model/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BorrowLend.Model
+{
+
+    public class Item
+    {
+        int Id;
+        string Name;
+        string Borrower;
+        string Lender;
+
+    }
+    public class ExpenseType
+    {
+        int Id;
+        string ExpenseTypeName;
+    }
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+        public bool ShowRequestId => string.IsNullorEmpty(RequestId);
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
